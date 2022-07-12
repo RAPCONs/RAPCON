cc6ded5e7d8fb018ba5d50b3134f70d4adfe0aea
Elizabeth <elisha.elizabeth@gmail.com>
2022-07-12 11:22:24 -0700

    updates to flight, customer models

--- src/auth/models/flight.js
+++ src/auth/models/flight.js
@@ -1,1 +1,60 @@
+'use strict';
+
 // flight database scheme what we want from the api to be stored in the database
+
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define('flight-info', {
+    airline: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    flightNumber: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    departureAirport: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    departureTime: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    departureGate: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    aircraftSpeed: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    aircraftVerticalSpeed: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    arrivalAirport: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    arrivalTime: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    arrivalGate: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    baggageClaim: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    flightStatus: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    uuid: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};
--- src/auth/models/index.js
+++ src/auth/models/index.js
@@ -1,2 +1,39 @@
+'use strict';
+
 // this file brings all the models together to start the sequlize database 
 //  this is where we do the relational foreign key business DONT FORGET
+
+const { Sequelize, DataTypes } = require('sequelize');
+const flightSchema = require('./flight');
+const customerSchema = require('./user');
+require('dotenv').config();
+
+const DATABASE_URL = process.env.DATABASE_URL || 'postgres://localhost:5432/rapcon';
+
+const sequelize = new Sequelize (DATABASE_URL);
+
+// Circle back to this below when deploying to database
+// const sequelize = new Sequelize(DATABASE_URL, {
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false,
+//     },
+//   },
+// });
+
+// Flight Info
+const Flights = flightSchema(sequelize, DataTypes);
+
+// User Info
+const Customers = customerSchema(sequelize, DataTypes);
+
+// Create Associations
+Customers.hasMany(Flights, {foreignKey:'uuid', sourceKey:'uuid'});
+Flights.belongsTo(Customers, {foreignKey: 'uuid', targetKey: 'uuid'});
+
+module.exports = {
+  sequelize,
+  Flights,
+  Customers,
+};
