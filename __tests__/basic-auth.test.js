d9ce9902d311d78eb213c07d3afaade40f121bf9
Beau Hibbert <beauhibbert789@gmail.com>
2022-07-12 17:26:04 -0700

    auth tests in progress

---
+++ __tests__/basic-auth.test.js
@@ -0,0 +1,26 @@
+// 'use strict';
+
+// const supertest = require('supertest');
+// const request = supertest(app)
+// const base64 = require('base-64');
+// const sequelize = require('sequelize');
+
+
+// let userInfo = {
+//   admin: { username: 'admin-basic', password: 'password' },
+// };
+
+// beforeAll(async () => {
+//   await sequelize.sync();
+//   await users.create(userInfo.admin);
+// });
+// afterAll(async () => {
+//   await sequelize.drop();
+// });
+
+
+// describe('Auth Middle', () => {
+//   test('Able to sign in'), async () => {
+//     const  
+//   }
+// })
